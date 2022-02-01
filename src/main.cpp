bb563c50ed19d3e2971a7fc7696db275e93c470b
Elliot Stjernqvist <elliot.stjernqvist@gmail.com>
2022-02-01 16:09:14 +0100

    Defined pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
 #include <Arduino.h>
 
+//Motor control pins
+const int motorVelocity = 8;
+const int motorVelocity = 9;
+
+const int leftControl1 = 10;
+const int leftControl2 = 11;
+
+const int rightControl1 = 12;
+const int rightControl2 = 13;
+
+//Ultrasonic sensor pins
+const int trigFront = A0;
+const int echoFront = A1;
+
+const int trigRight = A2;
+const int echoRight = A3;
+
+const int trigLeft = A4;
+const int echoLeft = A5;
+
+
+
 void setup() {
   // put your setup code here, to run once:
 }
